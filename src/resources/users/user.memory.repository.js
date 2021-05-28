bc4556181dea0d7e1d997eb4e8c74d9bbd987ff3
alekseishkel <lexashkel@gmail.com>
2021-05-28 12:49:11 +0300

    docs: add user.repository doc

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,13 +1,33 @@
 const users = [];
 
+/**
+ * Gets all the users from the "database"
+ * @returns {Promise<Array<User>>} Promise object represents the array of users
+ */
 const getAllUsers = async () => users;
 
+/**
+ * Gets the user by id from the "database"
+ * @param {string} id of the requested user 
+ * @returns {Promise<User>} Promise object represents the user
+ */
 const getUser = async (id) => users.find(user => user.id === id);
 
+/**
+ * Add a new user to the "database"
+ * @param {object} user - User
+ * @returns {Promise<users.length>} - new length of users array
+ */
 const addUser = async (user) => {
   users.push(user);
 };
 
+/**
+ * Updates a user depending on the received data
+ * @param {string} id of updating user
+ * @param {User} User - the new user
+ * @returns {Promise<User>} Promise object represents the updated user
+ */
 const updateUser = async (id, {name, login, password}) => {
   let updatedUser;
   const updatingUser = await getUser(id);
@@ -23,6 +43,11 @@ const updateUser = async (id, {name, login, password}) => {
   return updatedUser;
 };
 
+/**
+ * Deletes the user from the "database"
+ * @param {string} id of deleting user
+ * @returns {Promise<User>} Promise object represents the deleted user
+ */
 const deleteUser = async (id) => {
   const deletingUser = await getUser(id);
 
