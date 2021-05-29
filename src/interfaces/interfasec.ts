ca318dbb19783e9cde4e2f5ca5a5fae2c41193ff
alekseishkel <lexashkel@gmail.com>
2021-05-29 13:15:17 +0300

    feat: add interfaces

---
+++ src/interfaces/interfasec.ts
@@ -0,0 +1,28 @@
+interface IBoard {
+  id: string,
+  title: string,
+  columns: Array<IColumn>,
+};
+
+interface IColumn {
+  id: string,
+  title: string,
+  order: number,
+};
+
+interface ITask {
+  id: string,
+  title: string,
+  order: number,
+  description: string,
+  userId: string,
+  boardId: string,
+  columnId: string,
+};
+
+interface IUser {
+  id: string,
+  name: string,
+  login: string,
+  password: string,
+};
